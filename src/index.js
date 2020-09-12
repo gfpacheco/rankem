d2691ade6cdd8f8e0c3fab6014de28e67beee57d
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2020-09-12 03:14:25 -0300

    Add usage

--- src/index.js
+++ src/index.js
@@ -17,4 +17,15 @@ async function run(scoresFilePath, test, ranksFilePath) {
 
 const [, , scoresFilePath, flag, ranksFilePath] = process.argv;
 
+if (!scoresFilePath || !['-t', '-o'].includes(flag) || !ranksFilePath) {
+  console.log(`Usage: yarn start SCORES_FILE_PATH FLAG RANKS_FILE_PATH
+
+    FLAG is one of:
+
+        -t  test computed ranks against ranks from RANKS_FILE_PATH
+        -o  output computed ranks to RANKS_FILE_PATH
+`);
+  return;
+}
+
 run(scoresFilePath, flag === '-t', ranksFilePath).catch(console.error);
